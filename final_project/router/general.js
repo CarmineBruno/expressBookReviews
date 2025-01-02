46951dc6fc1a246eb5dddc03b2bb35ffebd3ddad
CarmineBruno <bruno1.carmine@gmail.com>
2025-01-02 09:10:33 -0500

    Tasks10-14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,26 +32,58 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve()
+        },6000)});
+
+    myPromise.then(() => {
+        res.send(JSON.stringify(books,null,4));
+    })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]); });
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve()
+        },6000)});
+
+    myPromise.then(() => {
+        const isbn = req.params.isbn;
+        res.send(books[isbn]); 
+       })
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-const author = req.params.author;
-const booksList = Object.values(books).filter(b=> b.author === author)
-res.send(JSON.stringify(booksList,null,4));
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve()
+        },6000)});
+
+    myPromise.then(() => {
+        const author = req.params.author;
+        const booksList = Object.values(books).filter(b=> b.author === author)
+        res.send(JSON.stringify(booksList,null,4));
+       })
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    const booksList = Object.values(books).filter(b=> b.title === title)
-    res.send(JSON.stringify(booksList,null,4));});
+    const myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          resolve()
+        },6000)});
+
+    myPromise.then(() => {
+        const title = req.params.title;
+        const booksList = Object.values(books).filter(b=> b.title === title)
+        res.send(JSON.stringify(booksList,null,4));
+       })
+});
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
